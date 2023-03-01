ac352f522890294dad0d5f3a4213f85c6be71d46
Steven_Huasupoma <shuasupomaolaya@gmail.com>
2023-02-28 23:07:01 -0600

    creando method post

--- src/test/java/placeHolderTesting/CRUD.java
+++ src/test/java/placeHolderTesting/CRUD.java
@@ -4,6 +4,11 @@ import io.restassured.RestAssured;
 import io.restassured.config.EncoderConfig;
 import io.restassured.config.RestAssuredConfig;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class CRUD {
     final  String url = "https://jsonplaceholder.typicode.com";
@@ -12,6 +17,12 @@ public class CRUD {
     public void setUp(){
         RestAssured.config = new RestAssuredConfig().encoderConfig(EncoderConfig.encoderConfig().defaultContentCharset("UTF-8"));
         RestAssured.baseURI=url;
+    }
+
+    @Test
+    public void a_create(){
+        Map<String, Object> placeHolder = new HashMap<>();
+
     }
 
 }
